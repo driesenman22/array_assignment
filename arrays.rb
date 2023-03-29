d45e648dd20721d41e4527f109ec05b72d0d63bd
Dylan Riesenman <djriesenman@gmail.com>
2023-03-29 10:32:49 -0600

    Add vaiables for strings, floats, booleans, and integers

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+mgs_characters = ["Snake", "Liquid", "Ocelot", "Eva",]
+puts mgs_characters[0]
+
+favorite_numbers = [8, 16, 32, 4]
+puts favorite_numbers[0]
+
+decimal_numbers = [1.1, 3.7, 4.9, 8.8]
+puts decimal_numbers[0]
+
+booleans = [true, false, true, false]
+puts booleans[0]
